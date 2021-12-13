3436eb3da4f5bd21bd24afa7dcf377f9857a755b
Falphir <73762798+Falphir@users.noreply.github.com>
2021-12-13 22:03:48 +0000

    13.12.2021

--- src/components/BannerSection.js
+++ src/components/BannerSection.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import '../App.css';
-import { Button } from './Button';
 import './BannerSection.css';
-import { Input } from 'antd';
+import { Input, DatePicker, Button} from 'antd';
+
 
 function BannerSection() {
+    const { RangePicker } = DatePicker;
+
     return (
+        
         <div className='banner-container'>
             {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
             <h1>FIND BOOK ENJOY</h1>
             <p>What are you waiting for?</p>
-            <div className="banner-btns">
-                <Input size="large" placeholder="large size"/>
+            <div className="banner-search">
+                <Input  className='input' placeholder="Pesquise aqui..."/>
+                <RangePicker  className="check-in-out"/>
+                <Button className='btn-search'>Search</Button>
             </div>
         </div>
     )
