52459d502ccb249a5516e4c0ba3977880309a7a7
Falphir <73762798+Falphir@users.noreply.github.com>
2021-12-14 14:34:45 +0000

    14.12.2021

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,20 +9,26 @@ function Cards() {
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
+          <CardItem
+              src='images/room.jpg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
             <CardItem
-              src='images/img-3.jpg'
+              src='images/room1.jpg'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='images/room2.jpg'
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='images/room3.jpg'
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
