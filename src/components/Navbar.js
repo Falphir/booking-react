913f6fb1fe023968db78dbc14ce67a3772ddbe4f
heyliceeee <alicedias2002@hotmail.com>
2022-01-02 00:00:19 +0000

    show/hide buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../assets/logo/logo_simples.png'
 
 function Navbar() {
+
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(false);
+  const [button, setButton] = useState(true);
   const [dashboardLink, setDashboardButton] = useState(true);
+  const [signUpLink, setSignUpButton] = useState(true);
+  const [signInLink, setSignInButton] = useState(true);
+  const [signOutLink, setSignOutButton] = useState(true);
   const [reservesLink, setReservesButton] = useState(true);
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+  const [userLogged, setUserLogged] = useState(true);
+
 
   const showButton = () => {
     if (window.innerWidth <= 960) {
@@ -29,7 +35,7 @@ function Navbar() {
   };
 
   const role = "admin";
-  const login = true;
+  const login = false;
 
   const showReservesButton = () => {
     if (login) {
@@ -48,13 +54,99 @@ function Navbar() {
     }
   };
 
-  useEffect(() => {
-    showButton();
-    showDashboardButton();
-    showReservesButton();
-  }, []);
+  /*   useEffect(() => {
+      showButton();
+      showDashboardButton();
+      showReservesButton();
+    }, []); */
 
   window.addEventListener('resize', showButton);
+
+  const onClickLogout = () => {
+    fetch('/auth/logout', {
+      headers: { 'Accept': 'application/json' }
+    })
+
+      .then((response) => response.json())
+
+      .then((response) => {
+        if (response.logout) {
+
+          setUserLogged(false);
+          window.location.reload(false);
+        }
+      })
+
+      .catch(() => {
+        setUserLogged(false);
+      })
+  }
+
+  useEffect(() => {
+
+    fetch('/auth/me', {
+      headers: { 'Accept': 'application/json' }
+    })
+
+      .then((response) => response.json())
+
+      .then((response) => {
+        //se scope do utilizador for == ao scope q tem permissão pra ver button
+        setUserLogged(response.decoded);
+
+        if (response.decoded == 'read-own-reserves' || response.decoded == 'create-reserve' || response.decoded == 'detail-reserve') {
+
+          //window.location.reload(false);
+
+          setDashboardButton(false);
+          setReservesButton(true);
+          setSignUpButton(false);
+          setSignInButton(false);
+          setSignOutButton(true);
+          console.log("user");
+
+
+        } else if (response.decoded == 'create-reserve' || response.decoded == 'detail-reserve' || response.decoded == 'update-reserve' ||
+          response.decoded == 'read-reserves' || response.decoded == 'delete-reserve' || response.decoded == 'create-room' ||
+          response.decoded == 'update-room' || response.decoded == 'read-reserve-client' || response.decoded == 'delete-room' || response.decoded == 'read-users') {
+
+          setDashboardButton(true);
+          setReservesButton(false);
+          setSignUpButton(false);
+          setSignInButton(false);
+          setSignOutButton(true);
+          console.log("admin");
+
+
+        } else if (response.decoded == 'update-reserve' || response.decoded == 'read-reserves' || response.decoded == 'delete-reserve' || response.decoded == 'create-room' ||
+          response.decoded == 'update-room' || response.decoded == 'read-reserve-client' || response.decoded == 'delete-room' || response.decoded == 'create-reserve' ||
+          response.decoded == 'detail-reserve') {
+
+          setDashboardButton(true);
+          setReservesButton(false);
+          setSignUpButton(false);
+          setSignInButton(false);
+          setSignOutButton(true);
+          console.log("editor");
+
+        } else {
+
+          setDashboardButton(false);
+          setReservesButton(false);
+          setSignUpButton(true);
+          setSignInButton(true);
+          setSignOutButton(false);
+        }
+      })
+
+      .catch(() => {
+        setUserLogged(false);
+      })
+  }, [])
+
+
+  if (!userLogged) { }
+
 
   return (
     <>
@@ -82,6 +174,16 @@ function Navbar() {
               </Link>
             </li>
 
+            {reservesLink &&
+              <li className='nav-item'>
+                <Link to='/reserves'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  My Reserves
+                </Link>
+              </li>}
+
             {dashboardLink &&
               <li className='nav-item'>
                 <Link to='/dashboard'
@@ -93,16 +195,16 @@ function Navbar() {
               </li>}
             <li hidden={login}>
               <Link
-                to='/sign-up'
+                to='/register'
                 className='nav-links-mobile'
                 onClick={closeMobileMenu}
               >
                 Sign Up
               </Link>
             </li>
-            <li hidden={login}>
+            <li hidden={login} >
               <Link
-                to='/sign-in'
+                to='/login'
                 className='nav-links-mobile'
                 onClick={closeMobileMenu}
               >
@@ -110,8 +212,23 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle='btn--gold--outline'>SIGN UP</Button>}
-          {button && <Button buttonStyle='btn--gold--outline'>SIGN IN</Button>}
+
+
+          {signUpLink &&
+            <Link to='/register' className='btn-mobile'>
+              <Button buttonStyle='btn--gold--outline'>SIGN UP</Button>
+            </Link>}
+
+
+          {signInLink &&
+            <Link to='/login' className='btn-mobile'>
+              <Button buttonStyle='btn--gold--outline'>SIGN IN</Button>
+            </Link>}
+
+          {signOutLink &&
+            <div className='btn-mobile'>
+              <Button buttonStyle='btn--gold--outline' onClick={onClickLogout}>SIGN OUT</Button>
+            </div>}
         </div>
       </nav>
     </>
